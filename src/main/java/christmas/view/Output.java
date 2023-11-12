9387dfe98bda0d3966659ed9df7c0595e2853849
ehddbs4521 <ehddbs452100@naver.com>
2023-11-12 18:24:57 +0900

    feat: 출력문을 enum을 사용해서 상수화

---
+++ src/main/java/christmas/view/Output.java
@@ -0,0 +1,24 @@
+package christmas.view;
+
+public enum Output {
+    START_PLANNER_MESSAGE("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."),
+    PRE_EVENT_BENEFIT_MESSAGE("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n"),
+    ORDER_LIST_MESSAGE("<주문 메뉴>\n"),
+    TOTAL_PRICE_BEFORE_SALE_MESSAGE("<할인 전 총주문 금액>\n"),
+    FREEBIE_MESSAGE("<증정 메뉴>\n"),
+    BENEFIT_CONTENT_MESSAGE("<혜택 내역>\n"),
+    TOTAL_BENEFIT_PRICE_MESSAGE("<총혜택 금액>\n"),
+    TOTAL_PRICE_AFTER_SALE_MESSAGE("<할인 후 예상 결제 금액>\n"),
+    EVENT_BADGE_MESSAGE("<12월 이벤트 배지>\n"),
+    ;
+
+    private final String message;
+
+    Output(String message) {
+        this.message = message;
+    }
+
+    public String get(){
+        return message;
+    }
+}

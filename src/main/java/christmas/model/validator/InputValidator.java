40d9a741b9c364c22838034408baf93f9df3d7ce
ehddbs4521 <ehddbs452100@naver.com>
2023-11-12 21:04:10 +0900

    feat: 입력한 메뉴와 갯수가 옳바른 패턴으로 입력했는지 검증

--- src/main/java/christmas/model/validator/InputValidator.java
+++ src/main/java/christmas/model/validator/InputValidator.java
@@ -1,16 +1,19 @@
 package christmas.model.validator;
 
+import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import static christmas.model.validator.ExceptionHandler.error;
-import static christmas.model.validator.ExceptionMessage.ORDER_LESS_THAN_ONE_MESSAGE;
-import static christmas.model.validator.ExceptionMessage.WRONG_DATE_MESSAGE;
+import static christmas.model.validator.ExceptionMessage.*;
 import static christmas.model.validator.IntegerConstant.END_DATE;
 import static christmas.model.validator.IntegerConstant.START_DATE;
+import static christmas.model.validator.Regex.CHECK_MENU_AND_COUNT;
 import static christmas.model.validator.Regex.CHECK_NUMERIC;
 
 public class InputValidator {
     private static final Pattern NUMERIC = Pattern.compile(CHECK_NUMERIC.get());
+    private static final Pattern MenuAndCount=Pattern.compile(CHECK_MENU_AND_COUNT.get());
     public void validateNumber(String input) {
         if (NUMERIC.matcher(input).matches()) return;
         error(ORDER_LESS_THAN_ONE_MESSAGE.get());
@@ -19,5 +22,11 @@ public class InputValidator {
     public void validateDate(int input) {
         if(input>=START_DATE.get() && input<= END_DATE.get()) return;
         error(WRONG_DATE_MESSAGE.get());
+    }
+
+    public void validateMenuAndCount(List<String> input) {
+        long count = input.stream().filter(str -> MenuAndCount.matcher(str).find()).count();
+        if(count==(long)input.size()) return;
+        error(ORDER_WRONG_MESSAGE.get());
     }
 }
--- src/main/java/christmas/model/validator/Regex.java
+++ src/main/java/christmas/model/validator/Regex.java
@@ -2,6 +2,7 @@ package christmas.model.validator;
 
 public enum Regex {
     CHECK_NUMERIC("^[1-9]\\d*"),
+    CHECK_MENU_AND_COUNT("^[가-힣]*-\\d\n")
     ;
     private final String message;
 

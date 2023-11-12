d29bcc903c6a06bc01b6c195a496ad43c718634c
ehddbs4521 <ehddbs452100@naver.com>
2023-11-12 20:31:33 +0900

    feat: 입력한 날짜가 조건에 부합한지 검증

--- src/main/java/christmas/model/validator/InputValidator.java
+++ src/main/java/christmas/model/validator/InputValidator.java
@@ -4,14 +4,20 @@ import java.util.regex.Pattern;
 
 import static christmas.model.validator.ExceptionHandler.error;
 import static christmas.model.validator.ExceptionMessage.ORDER_LESS_THAN_ONE_MESSAGE;
+import static christmas.model.validator.ExceptionMessage.WRONG_DATE_MESSAGE;
+import static christmas.model.validator.IntegerConstant.END_DATE;
+import static christmas.model.validator.IntegerConstant.START_DATE;
 import static christmas.model.validator.Regex.CHECK_NUMERIC;
 
 public class InputValidator {
     private static final Pattern NUMERIC = Pattern.compile(CHECK_NUMERIC.get());
     public void validateNumber(String input) {
-        if (NUMERIC.matcher(input).matches()) {
-            return;
-        }
+        if (NUMERIC.matcher(input).matches()) return;
         error(ORDER_LESS_THAN_ONE_MESSAGE.get());
+    }
+
+    public void validateDate(int input) {
+        if(input>=START_DATE.get() && input<= END_DATE.get()) return;
+        error(WRONG_DATE_MESSAGE.get());
     }
 }

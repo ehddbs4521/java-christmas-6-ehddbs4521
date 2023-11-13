910049cc684185cdccd2f284aeaf12c4861d934b
ehddbs4521 <ehddbs452100@naver.com>
2023-11-13 22:17:24 +0900

    feat: 할인 적용이 가능한지 검증

--- src/main/java/christmas/model/calculate/Price.java
+++ src/main/java/christmas/model/calculate/Price.java
@@ -5,6 +5,8 @@ import christmas.model.order.MenuAndPrice;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static christmas.model.validator.IntegerConstant.START_DISCOUNT_PRICE;
+
 public class Price {
     MenuAndPrice menuAndPrice = new MenuAndPrice();
     Map<String,Integer> order;
@@ -19,7 +21,11 @@ public class Price {
                 .map(menuEntry -> menuEntry.getValue() * order.get(menuEntry.getKey()))
                 .collect(Collectors.toList());
         return price.stream().mapToInt(Integer::intValue).sum();
+    }
 
+    public boolean getAfterDiscountPrice() {
+        if(getBeforeDiscountPrice()>=START_DISCOUNT_PRICE.get()) return true;
+        return false;
     }
 
 }

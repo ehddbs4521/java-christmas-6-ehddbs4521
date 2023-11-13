e56fbe1237121b6139c60a1003be7b3037e64aa6
ehddbs4521 <ehddbs452100@naver.com>
2023-11-13 15:39:29 +0900

    fix: 주문 형태 정규식 수정

--- src/main/java/christmas/model/validator/Regex.java
+++ src/main/java/christmas/model/validator/Regex.java
@@ -2,7 +2,7 @@ package christmas.model.validator;
 
 public enum Regex {
     CHECK_NUMERIC("^[1-9]\\d*"),
-    CHECK_MENU_AND_COUNT("^[가-힣]*-\\d\n")
+    CHECK_MENU_AND_COUNT("^[가-힣]*-\\d+$")
     ;
     private final String message;
 

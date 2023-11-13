231109903bc8c4e400ab4266687e8e7f78045f74
ehddbs4521 <ehddbs452100@naver.com>
2023-11-13 21:38:30 +0900

    fix: "/n" 제거

--- src/main/java/christmas/view/Output.java
+++ src/main/java/christmas/view/Output.java
@@ -3,8 +3,8 @@ package christmas.view;
 public enum Output {
     START_PLANNER_MESSAGE("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."),
     PRE_EVENT_BENEFIT_MESSAGE("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n"),
-    ORDER_LIST_MESSAGE("<주문 메뉴>\n"),
-    TOTAL_PRICE_BEFORE_SALE_MESSAGE("<할인 전 총주문 금액>\n"),
+    ORDER_LIST_MESSAGE("<주문 메뉴>"),
+    TOTAL_PRICE_BEFORE_SALE_MESSAGE("\n<할인 전 총주문 금액>"),
     FREEBIE_MESSAGE("<증정 메뉴>\n"),
     BENEFIT_CONTENT_MESSAGE("<혜택 내역>\n"),
     TOTAL_BENEFIT_PRICE_MESSAGE("<총혜택 금액>\n"),

e9c32ed5b66bd3cb86d3968fdc585e3802c2a46d
ehddbs4521 <ehddbs452100@naver.com>
2023-11-13 15:40:38 +0900

    feat: 메뉴와 갯수 주문 받기(controller)

---
+++ src/main/java/christmas/controller/StartController.java
@@ -0,0 +1,54 @@
+package christmas.controller;
+
+import christmas.model.order.Order;
+import christmas.model.order.VisitDate;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static christmas.model.validator.StringConstant.COMMA;
+
+
+public class StartController {
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+    VisitDate visitDate;
+    Order order;
+    private static String WEEKENDORNOT;
+
+    public void start() {
+        outputView.printWelcomeMessage();
+        askDate();
+        askMenuAndCount();
+    }
+
+    private void askDate() {
+        while (true) {
+            try {
+                String date = inputView.printVisitDateMessage();
+                visitDate = new VisitDate(date);
+                break;
+            } catch (IllegalArgumentException e) {
+                outputView.printMessage(e.getMessage());
+            }
+        }
+        WEEKENDORNOT = visitDate.weekendOrNot(visitDate.getDate());
+
+    }
+
+    private void askMenuAndCount() {
+        while (true) {
+            try {
+                String menuAndCount = inputView.printOrderAndCount();
+                new Order(Arrays.asList(menuAndCount.split(COMMA.get())));
+                break;
+            } catch (IllegalArgumentException e) {
+                outputView.printMessage(e.getMessage());
+            }
+        }
+    }
+
+
+}
